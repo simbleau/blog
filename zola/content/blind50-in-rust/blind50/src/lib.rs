20949c72e09a14c5be237fcae31bcf3d700040fb
Spencer Imbleau <spencer@imbleau.com>
2022-05-19 06:56:53 -0400

    Added two_sum

--- zola/content/blind50-in-rust/blind50/src/lib.rs
+++ zola/content/blind50-in-rust/blind50/src/lib.rs
@@ -2,5 +2,12 @@
 //
 // Authored by Spencer C. Imbleau
 #[allow(dead_code)]
-
 mod longest_substring_without_repeating_characters;
+mod two_sum;
+
+pub fn template() {}
+#[cfg(test)]
+#[test]
+fn test_1() {
+    assert_eq!(template(), ());
+}
