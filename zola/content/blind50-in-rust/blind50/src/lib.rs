3ee9c6af6a5692ccc49d6d202f589b437619c284
Spencer Imbleau <spencer@imbleau.com>
2022-05-19 07:35:39 -0400

    Added contains_duplicate

--- zola/content/blind50-in-rust/blind50/src/lib.rs
+++ zola/content/blind50-in-rust/blind50/src/lib.rs
@@ -1,6 +1,7 @@
 // This project contains the source for all Blind 75 problems.
 //
 // Authored by Spencer C. Imbleau
+mod contains_duplicate;
 #[allow(dead_code)]
 mod longest_substring_without_repeating_characters;
 mod two_sum;
