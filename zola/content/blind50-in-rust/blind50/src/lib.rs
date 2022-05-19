cfca7a050021b64a009d461344d5e2c709b49776
Spencer Imbleau <spencer@imbleau.com>
2022-05-19 06:12:06 -0400

    Added gitignore

--- zola/content/blind50-in-rust/blind50/src/lib.rs
+++ zola/content/blind50-in-rust/blind50/src/lib.rs
@@ -1,3 +1,3 @@
-/// This project contains the source for all Blind 75 problems.
-///
-/// Authored by Spencer C. Imbleau
+// This project contains the source for all Blind 75 problems.
+//
+// Authored by Spencer C. Imbleau
