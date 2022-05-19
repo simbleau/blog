a33f9dd537dc193cc0424e8230848ba9c5749d92
Spencer Imbleau <spencer@imbleau.com>
2022-05-19 18:17:39 -0400

    Removed warnings

--- zola/content/blind50-in-rust/blind50/src/lib.rs
+++ zola/content/blind50-in-rust/blind50/src/lib.rs
@@ -1,16 +1,15 @@
 // This project contains the source for all Blind 50 problems.
 //
 // Authored by Spencer C. Imbleau
-#[allow(dead_code)]
-//
-mod best_time_to_buy_and_sell_stock;
-mod contains_duplicate;
-mod longest_substring_without_repeating_characters;
-mod two_sum;
-mod valid_anagram;
-mod valid_parentheses;
-mod maximum_subarray;
+pub mod best_time_to_buy_and_sell_stock;
+pub mod contains_duplicate;
+pub mod longest_substring_without_repeating_characters;
+pub mod maximum_subarray;
+pub mod two_sum;
+pub mod valid_anagram;
+pub mod valid_parentheses;
 
+// Template for a  solution
 pub fn template() {}
 #[cfg(test)]
 #[test]
