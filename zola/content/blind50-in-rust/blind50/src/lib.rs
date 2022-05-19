e019879b0d3a97c96303b0fb43caa244c8fc0430
Spencer Imbleau <spencer@imbleau.com>
2022-05-19 13:12:07 -0400

    Added valid_parentheses

--- zola/content/blind50-in-rust/blind50/src/lib.rs
+++ zola/content/blind50-in-rust/blind50/src/lib.rs
@@ -1,12 +1,14 @@
 // This project contains the source for all Blind 50 problems.
 //
 // Authored by Spencer C. Imbleau
+#[allow(dead_code)]
+//
 mod best_time_to_buy_and_sell_stock;
 mod contains_duplicate;
-#[allow(dead_code)]
 mod longest_substring_without_repeating_characters;
 mod two_sum;
 mod valid_anagram;
+mod valid_parentheses;
 
 pub fn template() {}
 #[cfg(test)]
